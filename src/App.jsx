4d2acf6efcce53c998d5c9252a8f3a4c4f7f6c05
Mohamed Burhan <bern2727@gmail.com>
2022-08-21 10:27:50 +0800

    1. remove old code 2. remove unused import

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import {useEffect, useState} from 'react'
+import {useState} from 'react'
 import './styles/App.css'
 
 // ES6
@@ -30,26 +30,5 @@ const App = () => {
     </div>
   )
 }
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React SkolaCode
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
 
 export default App;
