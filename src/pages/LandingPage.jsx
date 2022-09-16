6c97b16b5ec7b4adcc0f48e3a9830ceb49123510
Mohamed Burhan <bern2727@gmail.com>
2022-09-16 11:42:38 +0800

    1. added a flex direction in a container style 2. landing page     a. save the todo in state and todo list     b. display the todo list in order

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -10,6 +10,7 @@ const LandingPage = () => {
    * 1. Create a input box - DONE
    * 2. Create a save button - DONE
    * 3. After click save, we need to save the record in a todo state
+   * 4. Display the saved todos
    * 
    * -- version v.0.2
    * a. Have a edit, delete and complete option
@@ -26,6 +27,7 @@ const LandingPage = () => {
    * todoList design = 
    * [
    *   {
+   *     id: alphanumeric = 'asadsad13122' 
    *     content: string = '',
    *     status: boolean = 1|2|3
    *   }
@@ -47,22 +49,34 @@ const LandingPage = () => {
    */
   const [todoList, setTodoList] = useState([])
 
+  const saveTheTodo = () => {
 
+    // DEEP clone
+    const clonedArr = [...todoList]
+
+    clonedArr.push(todo)
+
+    setTodoList(clonedArr)
+    setTodo("")
+  }
 
   return (
     <div className='container'>
 
       <div>
-        <TextField 
+        <TextField
+          onChange={(evt) => setTodo(evt.target.value)}
           id="outlined-basic" 
           label="Todo" 
           variant="outlined"
           placeholder='Enter your todo here...'
+          value={todo}
           sx={{
             width: 400,
           }}
         />
         <Button 
+          onClick={saveTheTodo}
           variant="contained" 
           size="large"
           sx={{
@@ -72,6 +86,14 @@ const LandingPage = () => {
         >
           SAVE
         </Button>
+      </div>
+
+      <div>
+        {
+          todoList.map((eachTodo, key) => (
+            <p key={key}>{eachTodo}</p>
+          ))
+        }
       </div>
     </div>
   )
